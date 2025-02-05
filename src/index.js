6868765edc7ee5220fdb984b143aabfd2fb760da
KoustavDev <koustav.mj.official@gmail.com>
2025-02-05 23:18:56 +0530

    Add CORS and cookie-parser middleware, update environment configuration, and implement error handling utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express";
+import app from "./app.js";
 
-const app = express();
 dotenv.config({
   path: "./env",
 });
@@ -17,6 +16,10 @@ connectDB()
     console.log("MONGO db connection failed !!! ", error);
   });
 
+
+
+
+  
 
 // Another way
 // ;((async () => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (fn) => {
+  // Function inside a function
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      res.status(error.code).json({ success: false, message: error.message });
+    }
+  };
+};
+
+export default asyncHandler;
+
+// Another method
+/*
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+    }
+}
+*/
