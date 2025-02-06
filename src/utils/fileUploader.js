4e4fbbfdde8c3af3f45696ea2fc39c17bb2d5993
KoustavDev <koustav.mj.official@gmail.com>
2025-02-06 20:44:02 +0530

    Add Cloudinary integration for file uploads and implement multer middleware for handling file storage

---
+++ src/utils/fileUploader.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloud = async (filePath) => {
+    try {
+        if(!filePath) return null;
+        const response = await cloudinary.uploader.upload(filePath, {resource_type: "auto"});
+        console.log("File has been uploaded to cloudinary ",response);
+        // After uploading the file delete the file from server.
+        fs.unlinkSync(filePath); // unlink means delete.
+        return response;
+    } catch (error) {
+      // If uploading is failed delete the file from server.
+      fs.unlinkSync(filePath);
+      return null;
+    }
+}
+
+export default uploadOnCloud;
